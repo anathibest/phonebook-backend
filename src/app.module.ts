6678cacd44ed15fe48e7034cba46a48b98b99938
anathi best <anathi.best96@gmail.com>
2021-09-15 11:54:02 +0200

    mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { PhonebookModule } from './phonebook/phonebook.module';
 		// MongooseModule.forRoot("mongodb+srv://anathibest:1234@Best@phonebook.gu113.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { autoCreate: true, }),
 		
 
-		MongooseModule.forRoot("mongodb+srv://anathibest:1234@Best@phonebook.mkuim.mongodb.net/phonebookdatabase?retryWrites=true&w=majority", { autoCreate: true }),
+		MongooseModule.forRoot("mongodb+srv://anathibest:1234@Best@phonebook.mkuim.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { autoCreate: true }),
 		PhonebookModule,
 	],
 	controllers: [],
